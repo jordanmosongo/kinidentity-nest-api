0fd437116c9ae31d69359da18ab80606cdab1d43
Jordan Mosongo <jordan@kinshasadigital.com>
2023-02-11 01:10:06 +0100

    fix: set auto load entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IndividModule } from './individ/individ.module';
-import { Role } from './entities/Role';
-import { User } from './entities/User';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { FamilyModule } from './family/family.module';
 import * as dotenv from 'dotenv';
-import { Family } from './family/entities/family.entity';
 import { DistrictModule } from './district/district.module';
-import { District } from './district/entities/district.entity';
 import { CommuneModule } from './commune/commune.module';
-import { Commune } from './commune/entities/commune.entity';
 import { QuarterModule } from './quarter/quarter.module';
-import { Quarter } from './quarter/entities/quarter.entity';
 import { StreetModule } from './street/street.module';
-import { Street } from './street/entities/street.entity';
 
 dotenv.config();
 @Module({
@@ -28,7 +21,7 @@ dotenv.config();
       username: 'root',
       password: process.env.PASSWORD,
       database: process.env.DATABASE,
-      entities: [User, Role, Family, District, Commune, Quarter, Street],
+      autoLoadEntities: true,
       synchronize: true,
     }),    
     IndividModule,
